a96d34f62ab4b894ac1f25757c68d98aeec66e56
Jonjo McKay <jonjo@jonjomckay.com>
2014-12-04 11:14:53 +0000

    Added getContentValue and getObjectData helper methods to Property and EngineValue collections

--- src/main/java/com/manywho/sdk/entities/run/EngineValueCollection.java
+++ src/main/java/com/manywho/sdk/entities/run/EngineValueCollection.java
@@ -1,11 +1,21 @@
 package com.manywho.sdk.entities.run;
 
+import com.manywho.sdk.entities.run.elements.type.ObjectCollection;
+
 import java.util.ArrayList;
 
 public class EngineValueCollection extends ArrayList<EngineValue> {
     public String getContentValue(String name) {
         if (!this.isEmpty()) {
             return this.stream().filter(c -> c.getDeveloperName().equals(name)).findFirst().orElse(new EngineValue()).getContentValue();
+        }
+
+        return null;
+    }
+
+    public ObjectCollection getObjectData(String name) {
+        if (!this.isEmpty()) {
+            return this.stream().filter(o -> o.getDeveloperName().equals(name)).findFirst().orElse(new EngineValue()).getObjectData();
         }
 
         return null;
