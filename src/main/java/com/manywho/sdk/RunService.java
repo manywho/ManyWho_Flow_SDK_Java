eb60eb51be1a0d60155abcc74fbb70c687f7a68d
Jonjo McKay <jonjo@jonjomckay.com>
2014-12-11 17:52:54 +0000

    Renamed methods in RunService to make a bit more sense

--- src/main/java/com/manywho/sdk/RunService.java
+++ src/main/java/com/manywho/sdk/RunService.java
@@ -14,13 +14,13 @@ import com.mashape.unirest.request.HttpRequestWithBody;
 public class RunService {
 
     // @todo Change this name, maybe?
-    public InvokeType sendListenerServiceResponse(AuthenticatedWho authenticatedWho, String tenantId, String callbackUri, ListenerServiceResponse listenerServiceResponse) throws Exception {
-        return this.sendResponse(authenticatedWho, tenantId, callbackUri, listenerServiceResponse);
+    public InvokeType sendEvent(AuthenticatedWho authenticatedWho, String tenantId, String callbackUri, ListenerServiceResponse listenerServiceResponse) throws Exception {
+        return this.executeCallback(authenticatedWho, tenantId, callbackUri, listenerServiceResponse);
     }
 
     // @todo Change this name, maybe?
-    public InvokeType sendServiceResponse(AuthenticatedWho authenticatedWho, String tenantId, String callbackUri, ServiceResponse serviceResponse) throws Exception {
-        return this.sendResponse(authenticatedWho, tenantId, callbackUri, serviceResponse);
+    public InvokeType sendResponse(AuthenticatedWho authenticatedWho, String tenantId, String callbackUri, ServiceResponse serviceResponse) throws Exception {
+        return this.executeCallback(authenticatedWho, tenantId, callbackUri, serviceResponse);
     }
 
     protected HttpRequestWithBody createHttpClient(AuthenticatedWho authenticatedWho, String tenantId, String callbackUri) {
@@ -30,7 +30,7 @@ public class RunService {
                 .header("Content-Type", "application/json");
     }
 
-    protected InvokeType sendResponse(AuthenticatedWho authenticatedWho, String tenantId, String callbackUri, Response response) throws Exception {
+    protected InvokeType executeCallback(AuthenticatedWho authenticatedWho, String tenantId, String callbackUri, Response response) throws Exception {
         String responseBody = this.createHttpClient(authenticatedWho, tenantId, callbackUri)
                 .body(new ObjectMapper().writeValueAsString(response))
                 .asString()
