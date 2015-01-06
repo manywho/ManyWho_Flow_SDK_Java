b8b9d906d9521ea59691f243072c4fb4c7c8197e
Jonjo McKay <jonjo@jonjomckay.com>
2015-01-06 21:18:58 +0000

    Added deserializing of AuthenticatedWho

--- src/main/java/com/manywho/sdk/utils/AuthorizationUtils.java
+++ src/main/java/com/manywho/sdk/utils/AuthorizationUtils.java
@@ -2,6 +2,13 @@ package com.manywho.sdk.utils;
 
 import com.manywho.sdk.entities.security.AuthenticatedWho;
 import com.manywho.sdk.enums.AuthenticatedWhoToken;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AuthorizationUtils {
     public static AuthenticatedWho createPublicUserAuthenticatedWho(String tenantId) {
@@ -15,6 +22,30 @@ public class AuthorizationUtils {
             setTenantName("UNKNOWN");
             setToken("NONE");
             setUserId("PUBLIC_USER");
+        }};
+    }
+
+    public static AuthenticatedWho deserialize(String token) {
+        List<NameValuePair> pairs = URLEncodedUtils.parse(token, Charset.forName("UTF-8"));
+        Map<String, String> parameters = new HashMap<>();
+        for (NameValuePair nameValuePair : pairs) {
+            parameters.put(nameValuePair.getName(), nameValuePair.getValue());
+        }
+
+        return new AuthenticatedWho() {{
+            setDirectoryId(parameters.get(AuthenticatedWhoToken.DirectoryId.toString()));
+            setDirectoryName(parameters.get(AuthenticatedWhoToken.DirectoryName.toString()));
+            setEmail(parameters.get(AuthenticatedWhoToken.Email.toString()));
+            setFirstName(parameters.get(AuthenticatedWhoToken.FirstName.toString()));
+            setIdentityProvider(parameters.get(AuthenticatedWhoToken.IdentityProvider.toString()));
+            setLastName(parameters.get(AuthenticatedWhoToken.LastName.toString()));
+            setManyWhoTenantId(parameters.get(AuthenticatedWhoToken.ManyWhoTenantId.toString()));
+            setManyWhoToken(parameters.get(AuthenticatedWhoToken.ManyWhoToken.toString()));
+            setManyWhoUserId(parameters.get(AuthenticatedWhoToken.ManyWhoUserId.toString()));
+            setTenantName(parameters.get(AuthenticatedWhoToken.TenantName.toString()));
+            setToken(parameters.get(AuthenticatedWhoToken.Token.toString()));
+            setUserId(parameters.get(AuthenticatedWhoToken.UserId.toString()));
+            setUsername(parameters.get(AuthenticatedWhoToken.Username.toString()));
         }};
     }
 
