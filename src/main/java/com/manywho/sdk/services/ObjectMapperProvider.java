b2a8c957dc8d11249699ce3ccdee1fce990355b4
Jonjo McKay <jonjo@jonjomckay.com>
2015-02-27 11:03:41 +0000

    Made ObjectMapperProvider a factory too, for use with @Inject

--- src/main/java/com/manywho/sdk/services/ObjectMapperProvider.java
+++ src/main/java/com/manywho/sdk/services/ObjectMapperProvider.java
@@ -1,12 +1,13 @@
 package com.manywho.sdk.services;
 
 import com.fasterxml.jackson.databind.*;
+import org.glassfish.hk2.api.Factory;
 
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
+public class ObjectMapperProvider implements ContextResolver<ObjectMapper>, Factory<ObjectMapper> {
     private final ObjectMapper defaultObjectMapper;
 
     public ObjectMapperProvider() {
@@ -21,7 +22,17 @@ public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
     public static ObjectMapper getObjectMapper() {
         return new ObjectMapper()
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
                 .enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
                 .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
+    }
+
+    @Override
+    public ObjectMapper provide() {
+        return getObjectMapper();
+    }
+
+    @Override
+    public void dispose(ObjectMapper objectMapper) {
     }
 }
