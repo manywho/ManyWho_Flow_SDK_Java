d5dd71cf2be0a3acca1179583629a59a34917734
Jonjo McKay <jonjo@jonjomckay.com>
2015-03-05 11:59:41 +0000

    Added @JsonCreator method to AuthenticationType enum

--- src/main/java/com/manywho/sdk/enums/AuthenticationType.java
+++ src/main/java/com/manywho/sdk/enums/AuthenticationType.java
@@ -1,5 +1,7 @@
 package com.manywho.sdk.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum AuthenticationType {
     Public("PUBLIC"),
     AllUsers("ALL_USERS"),
@@ -14,5 +16,16 @@ public enum AuthenticationType {
     @Override
     public String toString() {
         return text;
+    }
+
+    @JsonCreator
+    public static AuthenticationType forValue(String value) {
+        for (AuthenticationType authenticationType : values()) {
+            if (value.equalsIgnoreCase(authenticationType.text)) {
+                return authenticationType;
+            }
+        }
+
+        throw new IllegalArgumentException("No constant with text " + value + " found");
     }
 }
