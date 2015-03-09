dddc13e1ead487a8bc4006fe7f93f457f53b49cc
Jonjo McKay <jonjo@jonjomckay.com>
2015-03-09 11:40:34 +0000

    Added ObjectMapper to the dependency injection binder

--- src/main/java/com/manywho/sdk/services/ServiceBinder.java
+++ src/main/java/com/manywho/sdk/services/ServiceBinder.java
@@ -1,7 +1,9 @@
 package com.manywho.sdk.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.manywho.sdk.services.factories.ConfigurationValuesFactory;
 import com.manywho.sdk.services.providers.ConfigurationValuesFactoryProvider;
+import com.manywho.sdk.services.providers.ObjectMapperProvider;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.spi.internal.ValueFactoryProvider;
 
@@ -10,6 +12,7 @@ import javax.inject.Singleton;
 public class ServiceBinder extends AbstractBinder {
     @Override
     protected void configure() {
+        bindFactory(ObjectMapperProvider.class).to(ObjectMapper.class);
         bind(ConfigurationValuesFactoryProvider.class).to(ValueFactoryProvider.class).in(Singleton.class);
         bind(ConfigurationValuesFactory.class).to(ConfigurationValuesFactory.class).in(Singleton.class);
     }
