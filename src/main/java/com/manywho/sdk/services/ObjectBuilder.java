07328f00a1e199bba041dec971c2fe963800cb15
Jonjo McKay <jonjo@jonjomckay.com>
2015-06-07 15:44:18 +0100

    Implemented ObjectBuilder::buildListProperty() so now lists can be built :thumbsup:

--- src/main/java/com/manywho/sdk/services/ObjectBuilder.java
+++ src/main/java/com/manywho/sdk/services/ObjectBuilder.java
@@ -1,6 +1,7 @@
 package com.manywho.sdk.services;
 
 import com.manywho.sdk.entities.run.elements.type.Object;
+import com.manywho.sdk.entities.run.elements.type.ObjectCollection;
 import com.manywho.sdk.entities.run.elements.type.PropertyCollection;
 import com.manywho.sdk.services.annotations.Id;
 import com.manywho.sdk.services.annotations.Property;
@@ -8,6 +9,9 @@ import com.manywho.sdk.services.annotations.Type;
 import org.modelmapper.ModelMapper;
 
 import java.lang.reflect.Field;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
 
 public class ObjectBuilder {
     public <T> Object build(java.lang.Object object, Class<T> tClass) throws Exception {
@@ -71,11 +75,26 @@ public class ObjectBuilder {
     }
 
     private com.manywho.sdk.entities.run.elements.type.Property buildListProperty(Field field, String developerName, java.lang.Object entity) throws Exception {
-        throw new Exception("Building list properties is not supported yet");
-//        return new com.manywho.sdk.entities.run.elements.type.Property(developerName, build(entity, field.getType()));
+        java.lang.Object fieldValue = getFieldValue(field, entity);
+
+        ObjectCollection objectCollection = new ObjectCollection() {{
+            if (fieldValue != null) {
+                for (java.lang.Object item : (Collection) fieldValue) {
+                    add(build(item, item.getClass()));
+                }
+            }
+        }};
+
+        return new com.manywho.sdk.entities.run.elements.type.Property(developerName, objectCollection);
     }
 
     private com.manywho.sdk.entities.run.elements.type.Property buildScalarProperty(Field field, String developerName, java.lang.Object entity) {
-        return new com.manywho.sdk.entities.run.elements.type.Property(developerName, getFieldValue(field, entity));
+        java.lang.Object value = getFieldValue(field, entity);
+
+        if (value instanceof Date) {
+            value = new SimpleDateFormat("MM/dd/yyyy H:m:s a").format(value);
+        }
+        
+        return new com.manywho.sdk.entities.run.elements.type.Property(developerName, value);
     }
 }
