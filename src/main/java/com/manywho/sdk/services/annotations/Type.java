9aa996290fda016cd3d71656905950fa10764446
Jonjo McKay <jonjo.mckay@manywho.com>
2015-11-10 16:07:43 +0000

    Added default values to the @Type annotation

--- src/main/java/com/manywho/sdk/services/annotations/Type.java
+++ src/main/java/com/manywho/sdk/services/annotations/Type.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Type {
     String value() default "";
-    String name();
-    String summary();
+    String name() default "";
+    String summary() default "";
 }
