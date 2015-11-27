5973f7a4e653cd0f26c775e6275f2191f393998e
Jonjo McKay <jonjo.mckay@manywho.com>
2015-11-27 10:54:02 +0000

    Added methods to create a stream from Iterable<T>

--- sdk-core/src/main/java/com/manywho/sdk/utils/StreamUtils.java
+++ sdk-core/src/main/java/com/manywho/sdk/utils/StreamUtils.java
@@ -5,6 +5,14 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class StreamUtils {
+    public static <T> Stream<T> asStream(Iterable<T> iterable) {
+        return asStream(iterable.iterator());
+    }
+
+    public static <T> Stream<T> asStream(Iterable<T> iterable, boolean parallel) {
+        return asStream(iterable.iterator(), true);
+    }
+
     public static <T> Stream<T> asStream(Iterator<T> iterator) {
         return asStream(iterator, false);
     }
