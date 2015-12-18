29cf1954817c1f8a84ab98b33a8c89e67587aa5f
Jonjo McKay <jonjo.mckay@manywho.com>
2015-12-18 16:20:39 +0000

    Removed unused static constructor from sdk-client TestConfig

--- sdk-client/src/test/java/com/manywho/sdk/client/TestConfig.java
+++ sdk-client/src/test/java/com/manywho/sdk/client/TestConfig.java
@@ -1,22 +1,6 @@
 package com.manywho.sdk.client;
 
-import com.google.common.io.Resources;
-
-import java.io.IOException;
-import java.util.Properties;
-
 final class TestConfig {
-    private static Properties properties;
-
-    static {
-        try {
-            properties = new Properties();
-            properties.load(Resources.getResource("config.properties").openStream());
-        } catch (IOException exception) {
-            throw new IllegalStateException("Couldn't open \"src/test/resources/config.properties", exception);
-        }
-    }
-
     public static String getTenantId() {
         return "72236525-545d-4927-bb53-37a58a79178e";
     }
