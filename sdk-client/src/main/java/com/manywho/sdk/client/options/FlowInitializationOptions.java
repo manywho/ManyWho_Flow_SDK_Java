07f5260dd4135bc0f88baba95a9001c853714ba9
Jonjo McKay <jonjo@jonjomckay.com>
2015-12-24 14:22:13 +0000

    Added some more documentation to sdk-client

--- sdk-client/src/main/java/com/manywho/sdk/client/options/FlowInitializationOptions.java
+++ sdk-client/src/main/java/com/manywho/sdk/client/options/FlowInitializationOptions.java
@@ -5,10 +5,21 @@ import com.manywho.sdk.enums.FlowMode;
 public class FlowInitializationOptions {
     private FlowMode mode;
 
+    /**
+     * Gets the desired mode to run the Flow as
+     *
+     * @return the desired mode to run the Flow as
+     */
     public FlowMode getMode() {
         return mode;
     }
 
+    /**
+     * Set the mode to run the Flow as. See {@link FlowMode} for the available modes
+     *
+     * @param mode the mode to run the Flow as
+     * @return this {@link FlowInitializationOptions} object for fluent building
+     */
     public FlowInitializationOptions setMode(FlowMode mode) {
         this.mode = mode;
         return this;
