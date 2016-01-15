898f17ee2e233b2fddee4e818a90297f58ef3ca8
Jonjo McKay <jonjo.mckay@manywho.com>
2016-01-15 15:40:46 +0000

    Fixed signatures of AbstractSocialController to match with what ManyWho expects

--- sdk-core/src/main/java/com/manywho/sdk/services/controllers/AbstractSocialController.java
+++ sdk-core/src/main/java/com/manywho/sdk/services/controllers/AbstractSocialController.java
@@ -1,79 +1,93 @@
 package com.manywho.sdk.services.controllers;
 
+import com.fasterxml.jackson.databind.node.TextNode;
 import com.manywho.sdk.entities.run.elements.config.SocialServiceRequest;
 import com.manywho.sdk.entities.social.MentionedWho;
 import com.manywho.sdk.entities.social.Message;
 import com.manywho.sdk.entities.social.MessageList;
 import com.manywho.sdk.entities.social.Who;
+import com.manywho.sdk.services.annotations.AuthorizationRequired;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public abstract class AbstractSocialController {
+public abstract class AbstractSocialController extends AbstractController {
 
     @Path("/social/stream")
     @POST
-    public abstract String createStream(SocialServiceRequest serviceRequest) throws Exception;
+    @AuthorizationRequired
+    public abstract TextNode createStream(SocialServiceRequest serviceRequest) throws Exception;
 
     @Path("/social/stream/{id}")
     @POST
-    public abstract MessageList getStreamMessages(SocialServiceRequest serviceRequest, @PathParam("id") String id)
+    @AuthorizationRequired
+    public abstract MessageList getStreamMessages(SocialServiceRequest serviceRequest, @PathParam("id") String streamId)
             throws Exception;
 
     @Path("/social/stream/{id}/user/me")
     @POST
-    public abstract Who getCurrentUser(SocialServiceRequest serviceRequest, @PathParam("id") String id)
+    @AuthorizationRequired
+    public abstract Who getCurrentUser(SocialServiceRequest serviceRequest, @PathParam("id") String streamId)
             throws Exception;
 
     @Path("/social/stream/{id}/user/name/{name}")
     @POST
+    @AuthorizationRequired
     public abstract List<MentionedWho> searchUsersByName(SocialServiceRequest serviceRequest,
-                                                         @PathParam("id") String id,
+                                                         @PathParam("id") String streamId,
                                                          @PathParam("name") String name)
             throws Exception;
 
     @Path("/social/stream/{id}/user/{userId}")
     @POST
-    public abstract Who getUser(SocialServiceRequest serviceRequest, @PathParam("id") String id,
+    @AuthorizationRequired
+    public abstract Who getUser(SocialServiceRequest serviceRequest, @PathParam("id") String streamId,
                                 @PathParam("userId") String userId)
             throws Exception;
 
     @Path("/social/stream/{id}/follower")
     @POST
-    public abstract List<Who> getStreamFollowers(SocialServiceRequest serviceRequest, @PathParam("id") String id)
+    @AuthorizationRequired
+    public abstract List<Who> getStreamFollowers(SocialServiceRequest serviceRequest, @PathParam("id") String streamId)
             throws Exception;
 
     @Path("/social/stream/{id}/share")
     @POST
-    public abstract Message shareMessage(@PathParam("id") String id) throws Exception;
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @AuthorizationRequired
+    public abstract Message shareMessage(@FormDataParam("serviceRequest") SocialServiceRequest serviceRequest,
+                                         @PathParam("id") String streamId) throws Exception;
 
     @Path("/social/stream/{id}/message")
     @POST
-    public abstract Message postNewMessage(@PathParam("id") String id) throws Exception;
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @AuthorizationRequired
+    public abstract Message postNewMessage(@FormDataParam("serviceRequest") SocialServiceRequest serviceRequest,
+                                           @PathParam("id") String streamId) throws Exception;
 
     @Path("/social/stream/{id}/message/{messageId}/like/{like}")
     @POST
-    public abstract String likeMessage(SocialServiceRequest serviceRequest, @PathParam("id") String id,
+    @AuthorizationRequired
+    public abstract TextNode likeMessage(SocialServiceRequest serviceRequest, @PathParam("id") String streamId,
                                        @PathParam("messageId") String messageId,
-                                       @PathParam("like") String like)
+                                       @PathParam("like") boolean like)
             throws Exception;
 
     @Path("/social/stream/{id}/message/{messageId}")
     @POST
-    public abstract String deleteMessage(SocialServiceRequest serviceRequest, @PathParam("id") String id,
+    @AuthorizationRequired
+    public abstract TextNode deleteMessage(SocialServiceRequest serviceRequest, @PathParam("id") String streamId,
                                        @PathParam("messageId") String messageId)
             throws Exception;
 
     @Path("/social/stream/{id}/follow/{follow}")
     @POST
-    public abstract String followStream(SocialServiceRequest serviceRequest, @PathParam("id") String id,
-                                      @PathParam("follow") String follow)
+    @AuthorizationRequired
+    public abstract TextNode followStream(SocialServiceRequest serviceRequest, @PathParam("id") String streamId,
+                                      @PathParam("follow") boolean follow)
             throws Exception;
 }
