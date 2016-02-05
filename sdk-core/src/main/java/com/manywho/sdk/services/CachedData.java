3527d289b4bba936ddcf00f210a353b27047421a
Jonjo McKay <jonjo.mckay@manywho.com>
2016-02-05 15:58:37 +0000

    Removed unused `CachedData.bodyStream` field

--- sdk-core/src/main/java/com/manywho/sdk/services/CachedData.java
+++ sdk-core/src/main/java/com/manywho/sdk/services/CachedData.java
@@ -3,6 +3,5 @@ package com.manywho.sdk.services;
 import org.reflections.Reflections;
 
 public class CachedData {
-    public static byte[] bodyStream;
     public static Reflections reflections;
 }
