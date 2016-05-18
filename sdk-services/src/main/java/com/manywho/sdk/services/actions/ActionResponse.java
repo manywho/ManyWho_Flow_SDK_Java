a4073d8cef7084ddef265ddfdd2e6015033e31cd
Jonjo McKay <jonjo.mckay@manywho.com>
2016-05-18 16:43:44 +0100

    Added constructors to $File for when an action has no outputs

--- sdk-services/src/main/java/com/manywho/sdk/services/actions/ActionResponse.java
+++ sdk-services/src/main/java/com/manywho/sdk/services/actions/ActionResponse.java
@@ -7,6 +7,15 @@ public class ActionResponse<O> {
     private InvokeType invokeType = InvokeType.Forward;
     private String waitMessage;
 
+    public ActionResponse(InvokeType invokeType) {
+        this.invokeType = invokeType;
+    }
+
+    public ActionResponse(InvokeType invokeType, String waitMessage) {
+        this.invokeType = invokeType;
+        this.waitMessage = waitMessage;
+    }
+
     public ActionResponse(O outputs) {
         this.outputs = outputs;
     }
@@ -22,12 +31,12 @@ public class ActionResponse<O> {
         this.waitMessage = waitMessage;
     }
 
-    public O getOutputs() {
-        return outputs;
-    }
-
     public InvokeType getInvokeType() {
         return invokeType;
+    }
+
+    public O getOutputs() {
+        return outputs;
     }
 
     public String getWaitMessage() {
