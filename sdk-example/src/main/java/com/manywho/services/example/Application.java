105d713444f931103b25dcc3f466ca443bb0caa8
Jonjo McKay <jonjo.mckay@manywho.com>
2016-09-02 16:50:37 +0100

    Made sdk-example Application in sync with the latest ServiceApplication

--- sdk-example/src/main/java/com/manywho/services/example/Application.java
+++ sdk-example/src/main/java/com/manywho/services/example/Application.java
@@ -1,22 +1,18 @@
 package com.manywho.services.example;
 
 import com.manywho.sdk.services.ServiceApplication;
-import io.undertow.Undertow;
-import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
 
 import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("/")
 public class Application extends ServiceApplication {
     public Application() {
-        this.setModule(new ApplicationModule());
         this.initialize();
     }
 
-    public static void main(String[] args) {
-        UndertowJaxrsServer server = new UndertowJaxrsServer();
-        Undertow.Builder serverBuilder = Undertow.builder().addHttpListener(8080, "0.0.0.0");
-        server.start(serverBuilder);
-        server.deploy(new Application(), "/");
+    public static void main(String[] args) throws Exception {
+        Application service = new Application();
+        service.setModule(new ApplicationModule());
+        service.startServer();
     }
 }
