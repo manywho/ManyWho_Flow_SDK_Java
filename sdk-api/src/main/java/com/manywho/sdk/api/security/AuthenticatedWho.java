d3eace8f304fc7c23783f6bfe340d67286f06464
Jonjo McKay <jonjo.mckay@manywho.com>
2016-09-05 14:21:40 +0100

    Added role and group information fields to AuthenticatedWho

--- sdk-api/src/main/java/com/manywho/sdk/api/security/AuthenticatedWho.java
+++ sdk-api/src/main/java/com/manywho/sdk/api/security/AuthenticatedWho.java
@@ -21,6 +21,10 @@ public class AuthenticatedWho {
     private String directoryId;
     private String firstName;
     private String lastName;
+    private String roleName;
+    private String roleId;
+    private String primaryGroupName;
+    private String primaryGroupId;
 
     public UUID getManyWhoUserId() {
         return manyWhoUserId;
@@ -132,6 +136,38 @@ public class AuthenticatedWho {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getPrimaryGroupName() {
+        return primaryGroupName;
+    }
+
+    public void setPrimaryGroupName(String primaryGroupName) {
+        this.primaryGroupName = primaryGroupName;
+    }
+
+    public String getPrimaryGroupId() {
+        return primaryGroupId;
+    }
+
+    public void setPrimaryGroupId(String primaryGroupId) {
+        this.primaryGroupId = primaryGroupId;
     }
 
     public static AuthenticatedWho createPublicUser(UUID tenant) {
