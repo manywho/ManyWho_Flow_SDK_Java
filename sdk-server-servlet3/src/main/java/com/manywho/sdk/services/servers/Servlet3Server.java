9d76f97e3a8249d84b987d78206d8661e8a85d25
Jonjo McKay <jonjo.mckay@manywho.com>
2016-09-21 11:39:27 +0100

    Removed stupid duplicate modules list from Servlet3Server

--- sdk-server-servlet3/src/main/java/com/manywho/sdk/services/servers/Servlet3Server.java
+++ sdk-server-servlet3/src/main/java/com/manywho/sdk/services/servers/Servlet3Server.java
@@ -1,19 +1,9 @@
 package com.manywho.sdk.services.servers;
 
-import com.google.common.collect.Lists;
-import com.google.inject.Module;
 import com.manywho.sdk.services.ServiceApplication;
-
-import java.util.List;
 
 public class Servlet3Server extends ServiceApplication implements Server {
     private Class<?> application;
-    private List<Module> modules = Lists.newArrayList();
-
-    @Override
-    public void addModule(Module module) {
-        modules.add(module);
-    }
 
     public void setApplication(Class<?> application) {
         this.application = application;
