841df1c251b6e31d468f315376c4de9b56e4bf5d
Jonjo McKay <jonjo.mckay@manywho.com>
2016-12-07 15:15:21 +0000

    Added a bunch of documentation to the Database interfaces

--- sdk-services/src/main/java/com/manywho/sdk/services/database/RawDatabase.java
+++ sdk-services/src/main/java/com/manywho/sdk/services/database/RawDatabase.java
@@ -7,7 +7,32 @@ import com.manywho.sdk.services.configuration.Configuration;
 
 import java.util.List;
 
+/**
+ * This interface is used when the service builder needs to handle raw ManyWho objects in their Database calls. This is
+ * normally only required if a dynamic Type is used instead of a statically-defined one.
+ *
+ * @param <C> the service's Configuration type
+ */
 public interface RawDatabase<C extends Configuration> extends WritableDatabase<C, MObject> {
+    /**
+     * Implementations of this method should look up a single item in the appropriate store with the given ID, and
+     * return a ManyWho object that includes the properties given in the <code>objectDataType</code> parameter.
+     *
+     * @param configuration  the populated Configuration object
+     * @param objectDataType information about the type of object that is expected to be returned by the Service
+     * @param id             the object's external ID
+     * @return a single ManyWho object
+     */
     MObject find(C configuration, ObjectDataType objectDataType, String id);
+
+    /**
+     * Implementations of this method should look up items in the appropriate store using the given filter, and return
+     * a list of ManyWho objects that include the properties given in the <code>objectDataType</code> parameter.
+     *
+     * @param configuration  the populated Configuration object
+     * @param objectDataType information about the type of objects that are expected to be returned by the Service
+     * @param filter         the filtering options to be used when querying for objects
+     * @return a list of ManyWho objects
+     */
     List<MObject> findAll(C configuration, ObjectDataType objectDataType, ListFilter filter);
 }
--- sdk-services/src/main/java/com/manywho/sdk/services/database/WritableDatabase.java
+++ sdk-services/src/main/java/com/manywho/sdk/services/database/WritableDatabase.java
@@ -4,16 +4,68 @@ import com.manywho.sdk.services.configuration.Configuration;
 
 import java.util.List;
 
+/**
+ * This interface is used when the service builder needs to handle write-only Database operations, so any Save or
+ * Delete action.
+ *
+ * @param <C> the service's Configuration type
+ * @param <T> the type of the ManyWho type to be used
+ */
 public interface WritableDatabase<C extends Configuration, T> {
+    /**
+     * Implementations of this method should take the object that is passed in, assign an ID to it, save it in the
+     * appropriate store then return the object again populated with the created ID.
+     *
+     * @param configuration the populated Configuration object
+     * @param object        the populated ManyWho object to create in the store
+     * @return a typed ManyWho object populated with an ID
+     */
     T create(C configuration, T object);
 
+    /**
+     * Implementations of this method should take the objects that are passed in, assign IDs to each one, save them in
+     * the appropriate store then return the list of objects again, populated with the created IDs.
+     *
+     * @param configuration the populated Configuration object
+     * @param objects       a list of populated ManyWho objects to create in the store
+     * @return a list of typed ManyWho objects populated with IDs
+     */
     List<T> create(C configuration, List<T> objects);
 
+    /**
+     * Implementations of this method should take the object that is passed in and delete it from the appropriate store.
+     *
+     * @param configuration the populated Configuration object
+     * @param object        the populated ManyWho object to delete from the store
+     */
     void delete(C configuration, T object);
 
+    /**
+     * Implementations of this method should take the objects that are passed in and delete them from the appropriate
+     * store.
+     *
+     * @param configuration the populated Configuration object
+     * @param objects       the populated ManyWho object to delete from the store
+     */
     void delete(C configuration, List<T> objects);
 
+    /**
+     * Implementations of this method should take the object passed in, update the existing record in the appropriate
+     * store, then return the now-updated object again.
+     *
+     * @param configuration the populated Configuration object
+     * @param object        the populated ManyWho object to update in the store
+     * @return a typed ManyWho object with, including any updated fields
+     */
     T update(C configuration, T object);
 
+    /**
+     * Implementations of this method should take the objects passed in, update the existing records in the appropriate
+     * store, then return the now-updated objects again.
+     *
+     * @param configuration the populated Configuration object
+     * @param objects       the populated ManyWho objects to update in the store
+     * @return a list of typed ManyWho objects, including any updated fields
+     */
     List<T> update(C configuration, List<T> objects);
 }
