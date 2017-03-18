cdaea8d8c8caa8378d22d38bd9c887b53b512d4f
Jonjo McKay <jonjo.mckay@manywho.com>
2017-03-18 16:11:24 +0000

    Readded static registration of JavaTimeModule

--- sdk-api/src/main/java/com/manywho/sdk/api/jackson/ObjectMapperFactory.java
+++ sdk-api/src/main/java/com/manywho/sdk/api/jackson/ObjectMapperFactory.java
@@ -3,6 +3,7 @@ package com.manywho.sdk.api.jackson;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ObjectMapperFactory {
     public static ObjectMapper create() {
@@ -12,6 +13,6 @@ public class ObjectMapperFactory {
                 .enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
                 .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
                 .enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS)
-                .findAndRegisterModules();
+                .registerModule(new JavaTimeModule());
     }
 }
