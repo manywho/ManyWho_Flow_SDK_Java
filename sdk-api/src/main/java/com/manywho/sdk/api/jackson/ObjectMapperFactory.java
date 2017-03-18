0d0e266d7678e7024c453e8cc64cc8fea4e56dfd
Jonjo McKay <jonjo.mckay@manywho.com>
2017-03-18 16:05:35 +0000

    Updated jackson dependency versions

--- sdk-api/src/main/java/com/manywho/sdk/api/jackson/ObjectMapperFactory.java
+++ sdk-api/src/main/java/com/manywho/sdk/api/jackson/ObjectMapperFactory.java
@@ -3,7 +3,6 @@ package com.manywho.sdk.api.jackson;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ObjectMapperFactory {
     public static ObjectMapper create() {
@@ -13,6 +12,6 @@ public class ObjectMapperFactory {
                 .enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
                 .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
                 .enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS)
-                .registerModule(new JavaTimeModule());
+                .findAndRegisterModules();
     }
 }
