2840281d48f6ee5e1a8a61816a4df3c930150ded
Jonjo McKay <jonjo@jonjomckay.com>
2017-04-13 18:21:21 +0100

    Added a provider for RunClient to sdk-services

--- sdk-server/src/main/java/com/manywho/sdk/services/ServiceApplicationModule.java
+++ sdk-server/src/main/java/com/manywho/sdk/services/ServiceApplicationModule.java
@@ -2,9 +2,11 @@ package com.manywho.sdk.services;
 
 import com.google.inject.AbstractModule;
 import com.manywho.sdk.api.security.AuthenticatedWho;
+import com.manywho.sdk.client.run.RunClient;
 import com.manywho.sdk.services.configuration.ApplicationConfiguration;
 import com.manywho.sdk.services.providers.AuthenticatedWhoProvider;
 import com.manywho.sdk.services.providers.ReflectionsProvider;
+import com.manywho.sdk.services.providers.RunClientProvider;
 import com.manywho.sdk.services.types.DummyTypeProvider;
 import com.manywho.sdk.services.types.TypeProvider;
 import org.jboss.resteasy.plugins.guice.RequestScoped;
@@ -20,6 +22,7 @@ public class ServiceApplicationModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(Reflections.class).toProvider(ReflectionsProvider.class).asEagerSingleton();
+        bind(RunClient.class).toProvider(RunClientProvider.class);
         bind(AuthenticatedWho.class).toProvider(AuthenticatedWhoProvider.class).in(RequestScoped.class);
 
         bind(TypeProvider.class).to(DummyTypeProvider.class);
