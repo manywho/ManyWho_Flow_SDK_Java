6b41129aebe4f1a43935ede41086788b9cd55aa4
Jonjo McKay <jonjo@jonjomckay.com>
2017-06-23 16:01:42 +0100

    Added executionMode field to ServiceRequest

--- sdk-api/src/main/java/com/manywho/sdk/api/run/elements/config/ServiceRequest.java
+++ sdk-api/src/main/java/com/manywho/sdk/api/run/elements/config/ServiceRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.Lists;
 import com.manywho.sdk.api.run.EngineValue;
+import com.manywho.sdk.api.run.FlowMode;
 import com.manywho.sdk.api.run.elements.map.OutcomeAvailable;
 
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class ServiceRequest extends AbstractRequest {
     private String uri;
     private List<EngineValue> inputs = Lists.newArrayList();
     private List<OutcomeAvailable> outcomes = Lists.newArrayList();
+    private FlowMode executionMode;
 
     public String getJoinPlayerUri() {
         return joinPlayerUri;
@@ -59,5 +61,13 @@ public class ServiceRequest extends AbstractRequest {
 
     public void setOutcomes(List<OutcomeAvailable> outcomes) {
         this.outcomes = MoreObjects.firstNonNull(outcomes, new ArrayList<OutcomeAvailable>());
+    }
+
+    public FlowMode getExecutionMode() {
+        return executionMode;
+    }
+
+    public void setExecutionMode(FlowMode executionMode) {
+        this.executionMode = executionMode;
     }
 }
