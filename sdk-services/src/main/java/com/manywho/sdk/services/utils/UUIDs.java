8eb4e75a53310745449c4064941301efe4a66df5
Jonjo McKay <jonjo.mckay@manywho.com>
2018-02-12 11:00:55 +0000

    Fixed UUID matching regex

--- sdk-services/src/main/java/com/manywho/sdk/services/utils/UUIDs.java
+++ sdk-services/src/main/java/com/manywho/sdk/services/utils/UUIDs.java
@@ -2,6 +2,6 @@ package com.manywho.sdk.services.utils;
 
 public class UUIDs {
     public static boolean isValid(String value) {
-        return value != null && value.trim().matches("/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/");
+        return value != null && value.trim().matches("[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}");
     }
 }
