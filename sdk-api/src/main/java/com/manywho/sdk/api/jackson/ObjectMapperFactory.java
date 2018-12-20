321632b8cd24b6729840e8a8f76aafca6a499f71
Jonjo McKay <jonjo.mckay@manywho.com>
2018-12-20 17:35:38 +0000

    Now sorting properties in API responses alphabetically

--- sdk-api/src/main/java/com/manywho/sdk/api/jackson/ObjectMapperFactory.java
+++ sdk-api/src/main/java/com/manywho/sdk/api/jackson/ObjectMapperFactory.java
@@ -1,6 +1,7 @@
 package com.manywho.sdk.api.jackson;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -11,6 +12,7 @@ public class ObjectMapperFactory {
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
                 .disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY)
                 .enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING)
                 .enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING)
                 .enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS)
