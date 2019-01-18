0bb1e69d87f4d85aeec1d53815c986db7955351b
Jonjo McKay <jonjo.mckay@manywho.com>
2019-01-18 16:15:51 +0000

    Making example configuration settings not required

--- sdk-example/src/main/java/com/manywho/services/example/ServiceConfiguration.java
+++ sdk-example/src/main/java/com/manywho/services/example/ServiceConfiguration.java
@@ -4,10 +4,10 @@ import com.manywho.sdk.api.ContentType;
 import com.manywho.sdk.services.configuration.Configuration;
 
 public class ServiceConfiguration implements Configuration {
-    @Configuration.Setting(name = "Username", contentType = ContentType.String)
+    @Configuration.Setting(name = "Username", contentType = ContentType.String, required = false)
     private String username;
 
-    @Configuration.Setting(name = "Password", contentType = ContentType.Password)
+    @Configuration.Setting(name = "Password", contentType = ContentType.Password, required = false)
     private String password;
 
     public String getUsername() {
